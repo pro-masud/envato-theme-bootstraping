149a46f58d34d03368d6d25f6601116d7441f32b
pro-masud <promasudbd@gmail.com>
2023-12-06 09:06:00 +0600

    basic theme setup theme text domain loding, add post-thumbnail, add html5, post-formats, add_editor_style and action hook is after_setup_theme

--- functions.php
+++ functions.php
@@ -1,1 +1,11 @@
-<?php 
+<?php
+function aximo_theme_setup() {
+    load_theme_textdomain("aximo");
+    add_theme_support("post-thumbnails");
+    add_theme_support("title-tag");
+    add_theme_support("html5", ['search-form', 'comment-list']);
+    add_theme_support("post-formats", ["image", "gallery", "quote", "audio", "video", "link"]);
+    add_editor_style("/assets/css/editor-style.css");
+}
+
+add_action("after_setup_theme", "aximo_theme_setup");
